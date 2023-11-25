a0958991e135b920a4c1c3059eb2b0ebde72e3f6
gabgcgt531g <gabgc.z930@gmail.com>
2023-11-25 13:51:24 +0100

    pruebas python

--- UT01/script.py
+++ UT01/script.py
@@ -1,2 +1,11 @@
-print ("hola mundo")
-print ("hola gabi")
+
+a = int(input("Ingrese número: "))
+b = int(input("Ingrese número: "))
+resul = a * b
+
+print ("El resultado es: ")
+
+if a > b:   
+   print (resul)
+else: 
+   print (resul + 5)
